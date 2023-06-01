8e6069c4f111c698f4b0d8179ed08581682d8a9c
Abhi-atg-1021 <abhisinghal112@gmail.com>
2023-06-01 18:15:36 +0530

    changes

---
+++ server/app.js
@@ -0,0 +1,32 @@
+const express= require('express');
+const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
+const router=require('./router/route');
+const cookeParser=require("cookie-parser");
+require('dotenv').config();
+
+
+// /**Database connection */
+require('./db/conn');
+
+const port = process.env.PORT||8000;
+
+// /**middleware */
+app.use(cookeParser());
+app.use(express.json());
+app.use(cors());//used for connection with react
+app.disable('x-powered-by'); // less hackers know about our stack
+app.use(morgan('tiny'));//HTTP request middleware logger
+
+/** HTTP GET Request */
+app.get('/', (req, res) => {
+    res.status(201).json("Home GET Request");
+});
+
+
+app.use(router);
+
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`);
+});
